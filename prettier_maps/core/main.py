c5a68a1362b9b24999f832624c9e44d660b83338
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2024-11-27 12:27:12 +0000

    feat: add more ui and some interfaces for interacting with quick osm

--- prettier_maps/core/main.py
+++ prettier_maps/core/main.py
@@ -1,1 +1,23 @@
+import inspect
+import sys
+from pathlib import Path
+
+from ..interfaces import ProcessQuickQueryProtocol
+
+process_quick_query: ProcessQuickQueryProtocol | None = None
+try:
+    print("Trying to import QuickOSM")
+    from qgis.utils import plugins  # type: ignore
+
+    quick_osm_dir = Path(inspect.getfile(plugins["QuickOSM"].__class__)).parent / "core"  # type: ignore
+    if quick_osm_dir and quick_osm_dir.exists():
+        sys.path.append(str(quick_osm_dir))
+
+    from process import process_quick_query  # type: ignore
+
+    assert isinstance(process_quick_query, ProcessQuickQueryProtocol)
+except Exception as e:
+    pass
+
+
 def generate_layers(prompt: str, create_new_map: bool) -> None: ...
--- prettier_maps/ui/dialog.py
+++ prettier_maps/ui/dialog.py
@@ -1,4 +1,5 @@
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QDialog, QHBoxLayout, QLineEdit, QPushButton, QVBoxLayout
 
 from ..core import generate_layers
@@ -8,6 +9,9 @@ class MainDialog(QDialog):  # type: ignore[misc]
     def __init__(self) -> None:
         super().__init__()
         self.init_ui()
+
+    def get_font(self) -> QFont:
+        return QFont("Arial", 12)
 
     def init_ui(self) -> None:
         self.setWindowTitle("Main Dialog")
@@ -19,15 +23,18 @@ class MainDialog(QDialog):  # type: ignore[misc]
         self.text_box.setPlaceholderText("Enter prompt")
         self.text_box.setAlignment(Qt.AlignmentFlag.AlignTop)
         self.text_box.setFixedHeight(100)
+        self.text_box.setFont(self.get_font())
         layout.addWidget(self.text_box, 0)
 
         button_layout = QHBoxLayout()
 
         self.add_button = QPushButton("Add to Existing Map", self)
+        self.add_button.setFont(self.get_font())
         self.add_button.clicked.connect(self.add_to_existing_map)
         button_layout.addWidget(self.add_button)
 
         self.create_button = QPushButton("Create New Map", self)
+        self.create_button.setFont(self.get_font())
         self.create_button.clicked.connect(self.create_new_map)
         button_layout.addWidget(self.create_button)
 
