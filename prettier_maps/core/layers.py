c6874fc3e60e452b106643781d92a58ea84f3abf
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2024-11-27 14:12:29 +0000

    fix script so it splits layers

--- prettier_maps/core/layers.py
+++ prettier_maps/core/layers.py
@@ -1,61 +1,80 @@
-from PyQt5.QtGui import QColor
+from collections import defaultdict
+
 from qgis.core import (
     QgsLayerTree,
-    QgsLayerTreeNode,
+    QgsLayerTreeGroup,
+    QgsLayerTreeLayer,
     QgsProject,
-    QgsRenderContext,
-    QgsVectorLayer,
     QgsVectorTileBasicRenderer,
+    QgsVectorTileBasicRendererStyle,
     QgsVectorTileLayer,
 )
 
-root = QgsProject.instance().layerTreeRoot()
+instance = QgsProject.instance()
+assert instance is not None
+root = instance.layerTreeRoot()
 assert root is not None
 
 
-def process_layer(
-    root: QgsLayerTree, layer: QgsVectorLayer | QgsVectorTileLayer | None
-):
-    """Process a single layer and print its properties"""
-    if isinstance(layer, QgsVectorLayer):
-        renderer = layer.renderer()
-        if renderer:
-            symbols = renderer.symbols(QgsRenderContext())
-            for symbol in symbols:
-                print(f"Layer: {layer.name()}")
-                print(f"Symbol: {symbol.dump()}")
-    elif isinstance(layer, QgsVectorTileLayer):
-        print(f"Vector Tile Layer: {layer.name()}")
-        renderer_for_vector_tile_layer: QgsVectorTileBasicRenderer = layer.renderer()
-        if renderer_for_vector_tile_layer:
-            styles = renderer_for_vector_tile_layer.styles()
+def get_styles_from_vector_tile_layer(
+    layer: QgsVectorTileLayer,
+) -> list[QgsVectorTileBasicRendererStyle]:
+    renderer = layer.renderer()
+    if renderer is None:
+        return []
+
+    assert isinstance(renderer, QgsVectorTileBasicRenderer)
+
+    styles = renderer.styles()
+    return styles  # type: ignore[no-any-return]
+
+
+def get_layers_from_group(group: QgsLayerTreeGroup) -> list[QgsLayerTreeLayer]:
+    return [layer for layer in group.children() if isinstance(layer, QgsLayerTreeLayer)]
+
+
+GroupName = str
+LayerName = str
 
 
 def iterate_groups_and_layers(
-    root: QgsLayerTree, group: QgsLayerTreeNode | None = None
+    root: QgsLayerTree,
 ):
-    """Recursively iterate through groups and layers"""
-    if group is None:
-        group = root
-    for child in group.children():
-        if child.nodeType() == QgsLayerTreeNode.NodeType.NodeGroup:
-            print(f"Group: {child.name()}")
-            iterate_groups_and_layers(root, child)
-        elif child.nodeType() == QgsLayerTreeNode.NodeType.NodeLayer:
-            print(f"Layer: {child.name()}")
-            layer = child.layer()
-            if layer:
-                process_layer(root, layer)
+    for child in root.children():
+        if not isinstance(child, QgsLayerTreeGroup):
+            continue
+
+        layer_mapping: dict[
+            GroupName, list[tuple[LayerName, QgsVectorTileBasicRendererStyle]]
+        ] = defaultdict(list)
+
+        vector_tile_layers = get_layers_from_group(child)
+        for layer in vector_tile_layers:
+            map_layer = layer.layer()
+            print(f"{layer!r}, {map_layer!r}")
+            if not isinstance(map_layer, QgsVectorTileLayer):
+                continue
+
+            styles = get_styles_from_vector_tile_layer(map_layer)
+
+            for style in styles:
+                symbol = style.symbol()
+                if symbol is None:
+                    continue
+                layer_mapping[style.layerName()].append((style.styleName(), style))
+
+        for new_group_name, styles in layer_mapping.items():
+            new_group = child.addGroup(new_group_name)
+            assert new_group is not None
+            for layer_name, style in styles:
+                new_layer = QgsVectorTileLayer(baseName=layer_name)
+                tree_layer = QgsLayerTreeLayer(new_layer)
+                renderer = QgsVectorTileBasicRenderer()
+                style_copy = QgsVectorTileBasicRendererStyle(style)
+                renderer.setStyles([style_copy])
+                new_layer.setRenderer(renderer)
+                new_group.addChildNode(tree_layer)
+                new_layer.triggerRepaint()
 
 
 iterate_groups_and_layers(root)
-
-
-def main_():
-    for layer in QgsProject.instance().mapLayers().values():
-        if isinstance(layer, QgsVectorTileLayer):
-            renderer: QgsVectorTileBasicRenderer = layer.renderer()
-            if renderer:
-                styles = renderer.styles()
-                for idx, style in enumerate(styles):
-                    print(f"Style {idx}: {style.layerName()}")
