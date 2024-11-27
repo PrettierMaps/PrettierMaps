91b284d3f0184388caab823d2e512782b012b74e
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2024-11-27 15:31:14 +0000

    add individual layers per symbol

--- prettier_maps/core/layers.py
+++ prettier_maps/core/layers.py
@@ -37,19 +37,19 @@ GroupName = str
 LayerName = str
 
 
-def iterate_groups_and_layers(
-    root: QgsLayerTree,
-):
+def iterate_groups_and_layers(root: QgsLayerTree):
+    proj = QgsProject().instance()
+    assert proj is not None
+
     for child in root.children():
         if not isinstance(child, QgsLayerTreeGroup):
             continue
 
-        layer_mapping: dict[
-            GroupName, list[tuple[LayerName, QgsVectorTileBasicRendererStyle]]
-        ] = defaultdict(list)
-
         vector_tile_layers = get_layers_from_group(child)
         for layer in vector_tile_layers:
+            layer_mapping: dict[GroupName, list[tuple[LayerName, int]]] = defaultdict(
+                list
+            )
             map_layer = layer.layer()
             print(f"{layer!r}, {map_layer!r}")
             if not isinstance(map_layer, QgsVectorTileLayer):
@@ -57,24 +57,37 @@ def iterate_groups_and_layers(
 
             styles = get_styles_from_vector_tile_layer(map_layer)
 
-            for style in styles:
+            for i, style in enumerate(styles):
                 symbol = style.symbol()
                 if symbol is None:
                     continue
-                layer_mapping[style.layerName()].append((style.styleName(), style))
+                layer_mapping[style.layerName()].append((style.styleName(), i))
 
-        for new_group_name, styles in layer_mapping.items():
-            new_group = child.addGroup(new_group_name)
-            assert new_group is not None
-            for layer_name, style in styles:
-                new_layer = QgsVectorTileLayer(baseName=layer_name)
-                tree_layer = QgsLayerTreeLayer(new_layer)
-                renderer = QgsVectorTileBasicRenderer()
-                style_copy = QgsVectorTileBasicRendererStyle(style)
-                renderer.setStyles([style_copy])
-                new_layer.setRenderer(renderer)
-                new_group.addChildNode(tree_layer)
-                new_layer.triggerRepaint()
+            for new_group_name, styles in layer_mapping.items():
+                new_group = child.addGroup(new_group_name)
+                assert new_group is not None
+                for layer_name, style_index in styles:
+                    new_map_layer = map_layer.clone()
+                    if new_map_layer is None:
+                        continue
+
+                    renderer = new_map_layer.renderer()
+                    if renderer is None:
+                        continue
+
+                    assert isinstance(renderer, QgsVectorTileBasicRenderer)
+
+                    current_styles = renderer.styles()
+                    renderer.setStyles([current_styles[style_index]])
+                    new_map_layer.setName(layer_name)
+                    proj.addMapLayer(new_map_layer, False)
+
+                    new_group.addLayer(new_map_layer)
+
+                print()
+
+                for layer in get_layers_from_group(new_group):
+                    print(layer, layer.layer())
 
 
 iterate_groups_and_layers(root)
