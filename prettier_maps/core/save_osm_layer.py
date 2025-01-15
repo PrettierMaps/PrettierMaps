0bb0771ae60ff3295be93232ce489580aaad2190
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-01-15 14:08:27 +0000

    add save layers button

--- prettier_maps/core/save_osm_layer.py
+++ prettier_maps/core/save_osm_layer.py
@@ -1,25 +1,43 @@
+from pathlib import Path
+
 from qgis.core import QgsProject, QgsVectorFileWriter, QgsVectorLayer
 
-# Define the output directory
-output_directory = r"C:\Users\ap_va\OneDrive\Documents\Irene - UofG\Year 3\PrettierMap"
 
-# Iterate through all layers in the QGIS project
-for layer in QgsProject.instance().mapLayers().values():
-    # Check if the layer is a vector layer and a temporary memory layer
-    if isinstance(layer, QgsVectorLayer) and layer.isValid() and layer.dataProvider().name() == 'memory':
-        # Define the output file path and layer name
-        print(layer)
-        layer_name = layer.name()
-        output_file = f"{output_directory}\\{layer_name}.gpkg"
-        
-        # Save the temporary layer to a GeoPackage
-        QgsVectorFileWriter.writeAsVectorFormat(layer, output_file, "UTF-8", layer.crs(), "GPKG", layerOptions=['GEOMETRY_NAME=geom', f'layerName={layer_name}'])
-        
-        # Add the permanent layer to the QGIS project
-        permanent_layer = QgsVectorLayer(f'{output_file}|layername={layer_name}', layer_name, "ogr")
-        QgsProject.instance().addMapLayer(permanent_layer)
-        
-        # Optionally, remove the temporary layer from the project
-        QgsProject.instance().removeMapLayer(layer.id())
+def save_quick_osm_layers(output_directory: str):
+    instance = QgsProject.instance()
+    assert instance is not None
 
-print("All temporary vector layers have been converted to permanent layers.")
+    for layer in instance.mapLayers().values():
+        # Check if the layer is a vector layer and a temporary memory layer
+        if (
+            isinstance(layer, QgsVectorLayer)
+            and layer.isValid()
+            and layer.dataProvider().name() == "memory"
+        ):
+            geom_type = layer.geometryType()
+            geom_type_str = ["point", "line", "polygon"][geom_type]
+
+            # Define the output file path and layer name
+            layer_name = layer.name()
+            new_layer_name = f"{layer_name}_{geom_type_str}"
+            layer.setName(new_layer_name)
+            output_file = str(Path(output_directory) / f"{new_layer_name}.gpkg")
+
+            # Save the temporary layer to a GeoPackage
+            QgsVectorFileWriter.writeAsVectorFormat(
+                layer,
+                output_file,
+                "UTF-8",
+                layer.crs(),
+                "GPKG",
+                layerOptions=["GEOMETRY_NAME=geom", f"layerName={new_layer_name}"],
+            )
+
+            # Add the permanent layer to the QGIS project
+            permanent_layer = QgsVectorLayer(
+                f"{output_file}|layername={new_layer_name}", new_layer_name, "ogr"
+            )
+            QgsProject.instance().addMapLayer(permanent_layer)
+
+            # Optionally, remove the temporary layer from the project
+            QgsProject.instance().removeMapLayer(layer.id())
