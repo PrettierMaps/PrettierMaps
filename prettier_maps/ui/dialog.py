7336eecc5485f3f9461c8ee14878ede080a1eab4
Deneb101 <danidmecha@gmail.com>
2025-02-05 14:22:28 +0000

    Commented dialog.py and splitted up init_ui into two functions

--- prettier_maps/ui/dialog.py
+++ prettier_maps/ui/dialog.py
@@ -27,29 +27,31 @@ class MainDialog(QDialog):  # type: ignore[misc]
         filter_layers(POSSIBLE_LAYERS)
 
     def get_font(self) -> QFont:
+        """Gets the font that this object will use.
+
+        :return: The font
+        """
+
         return QFont("Arial", 12)
 
-    def init_ui(self) -> None:
-        self.setWindowTitle("Prettier Maps")
-        self.resize(500, 600)
+    def build_layers_area(self) -> QScrollArea:
+        """Creates the scrolling area where the layer checkboxes will be listed
 
-        layout = QVBoxLayout()
-        layout.setContentsMargins(20, 20, 20, 20)
-        instructions = QLabel("Select Layers")
+        :return: The created scroll area
+        """
 
-        instructions.setFont(self.get_font())
-        instructions.setAlignment(Qt.AlignmentFlag.AlignLeft)
-        layout.addWidget(instructions)
-
+        # Create the scroll area
         scroll = QScrollArea()
         scroll.setWidgetResizable(True)
         scroll.setContentsMargins(0, 0, 0, 0)
         scroll.setMaximumHeight(500)
 
+        # Create the layout for the area
         layer_layout = QVBoxLayout()
         layer_layout.setSpacing(30)
         layer_layout.setContentsMargins(20, 20, 20, 20)
 
+        # Add a checkbox and name to represent each layer
         for layer in sorted(POSSIBLE_LAYERS):
             checkbox = QCheckBox(layer.title())
             checkbox.setFont(self.get_font())
@@ -61,8 +63,28 @@ class MainDialog(QDialog):  # type: ignore[misc]
         scroll_widget = QWidget()
         scroll_widget.setLayout(layer_layout)
         scroll.setWidget(scroll_widget)
-        layout.addWidget(scroll)
+        return scroll
 
+    def init_ui(self) -> None:
+        """Initializes the UI for this object"""
+
+        self.setWindowTitle("Prettier Maps")
+        self.resize(500, 600)
+
+        # Create the layout
+        layout = QVBoxLayout()
+        layout.setContentsMargins(20, 20, 20, 20)
+
+        # Add the basic instructions
+        instructions = QLabel("Select Layers")
+        instructions.setFont(self.get_font())
+        instructions.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        layout.addWidget(instructions)
+
+        # Add the layer checkboxes
+        layout.addWidget(self.build_layers_area())
+
+        # Add the Save Layers button
         file_layout = QHBoxLayout()
         save_button = QPushButton("Save Quick OSM Layers")
         save_button.setFont(self.get_font())
@@ -70,6 +92,7 @@ class MainDialog(QDialog):  # type: ignore[misc]
         file_layout.addWidget(save_button)
         layout.addLayout(file_layout)
 
+        # Add the close button
         close_button = QPushButton("Close")
         close_button.setFont(self.get_font())
         close_button.clicked.connect(self.close_dialog)
@@ -78,23 +101,33 @@ class MainDialog(QDialog):  # type: ignore[misc]
         self.setLayout(layout)
 
     def save_layers_dialog(self) -> None:
+        """Opens up a dialog to ask what directory to save the layers into, and then
+        saves the layers there.
+        """
+
         if not has_layers():
             QMessageBox.warning(self, "No Layers", "No layers found to save.")
             return
 
+        # Opens up the dialog
         dialog = QFileDialog()
         dialog.setFileMode(QFileDialog.FileMode.Directory)
         dialog.setOption(QFileDialog.Option.ShowDirsOnly, True)
 
+        # Saves the layers in the selected directory
         if dialog.exec_():
             folder_path = dialog.selectedFiles()[0]
             save_quick_osm_layers(folder_path)
             QMessageBox.information(
-                self, "Layers Saved",
-                "All OSM layers have been saved successfully."
+                self, "Layers Saved", "All OSM layers have been saved successfully."
             )
 
     def get_selected_layers(self) -> set[str]:
+        """Gets the layers whose checkbox is enabled
+
+        :return: A set of the layers names for the layers that are currently checked
+        """
+
         return {
             layer
             for layer, checkbox in self.layer_checkboxes.items()
@@ -102,8 +135,15 @@ class MainDialog(QDialog):  # type: ignore[misc]
         }
 
     def on_checkbox_changed(self, state: int) -> None:
+        """Reapplies the changes whenever a checkbox changes states
+
+        :param state: the new state of the checkbox. Argument is ignored
+        """
+
         selected = self.get_selected_layers()
         filter_layers(selected)
 
     def close_dialog(self) -> None:
+        """Closes the dialog"""
+
         self.close()
