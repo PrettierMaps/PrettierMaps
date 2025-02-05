f7e60665651d99f2e63c44f45af4a6ee795dd2b6
Deneb101 <danidmecha@gmail.com>
2025-02-05 13:10:31 +0000

    Commented and reformatted layers.py

--- prettier_maps/core/layers.py
+++ prettier_maps/core/layers.py
@@ -8,23 +8,50 @@ from qgis.core import (
 )
 
 
-def get_layers_from_group(group: "QgsLayerTreeGroup") -> list["QgsLayerTreeLayer"]:
+def get_layers_from_group(group: QgsLayerTreeGroup) -> list[QgsLayerTreeLayer]:
+    """Returns the direct descendant layers from a given QgsLayerTreeGroup.
+
+    :param group: The group to get the descendant layers from
+    :return: The direct descendant layers from group
+    """
+
     return [layer for layer in group.children() if isinstance(layer, QgsLayerTreeLayer)]
 
 
-def refresh_layer(layer: "QgsVectorTileLayer", renderer: "QgsVectorTileBasicRenderer"):
+def refresh_layer(
+    layer: QgsVectorTileLayer, renderer: QgsVectorTileBasicRenderer
+) -> None:
+    """Refreshes a certain layer.
+
+    :param layer: Layer to refresh
+    :param renderer: The renderer that will be used for the layer
+    """
+
     layer.setRenderer(renderer.clone())
     layer.setBlendMode(layer.blendMode())
     layer.setOpacity(layer.opacity())
 
 
-def _get_qgis_project() -> "QgsProject | None":
+def _get_qgis_project() -> Optional[QgsProject]:
+    """Returns the current QGIS project.
+
+    :return: The current QGIS project
+    """
+
     return QgsProject.instance()
 
 
 def filter_layers(
-    layers_to_turn_on: set[str], instance_to_filter: "QgsProject | None" = None
-):
+    layers_to_turn_on: set[str], instance_to_filter: Optional[QgsProject] = None
+) -> None:
+    """Given a set of layers, shows only those layers while hiding others.
+
+    :param layers_to_turn_on: Set of layers to be shown
+    :param instance_to_filter: Instance of a QGISProject to filter
+      on. If not provided, the current QGIS project is used instead.
+    """
+
+    # Select the project instance
     instance = instance_to_filter or _get_qgis_project()
     assert instance is not None
     root = instance.layerTreeRoot()
@@ -34,17 +61,19 @@ def filter_layers(
         if not isinstance(child, QgsLayerTreeGroup):
             continue
 
+        # Get the layer elements
         vector_tile_layers = get_layers_from_group(child)
         for layer in vector_tile_layers:
             map_layer = layer.layer()
             if not isinstance(map_layer, QgsVectorTileLayer):
                 continue
 
+            # Get the layer renderer
             renderer = map_layer.renderer()
             assert renderer is not None
-
             assert isinstance(renderer, QgsVectorTileBasicRenderer)
 
+            # Enable and disable the styles based on whether they're on the layers_to_turn_ on list
             styles = renderer.styles()
             new_styles: list[QgsVectorTileBasicRendererStyle] = []
             for style in styles:
@@ -55,4 +84,5 @@ def filter_layers(
                 new_styles.append(style)
 
             renderer.setStyles(new_styles)
+            # Lastly, refresh the layer
             refresh_layer(map_layer, renderer)
