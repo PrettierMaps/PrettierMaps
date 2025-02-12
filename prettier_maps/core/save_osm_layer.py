2af7ca74d82d2d746840aafe0c33ccde63290632
Deneb101 <danidmecha@gmail.com>
2025-02-12 14:54:51 +0000

    Removed a bunch of comments from save_osm_layer.py

--- prettier_maps/core/save_osm_layer.py
+++ prettier_maps/core/save_osm_layer.py
@@ -4,11 +4,6 @@ from qgis.core import QgsProject, QgsVectorFileWriter, QgsVectorLayer
 
 
 def has_layers() -> bool:
-    """Returns whether or not the current Project has any layers
-
-    :return: True if the current QGIS Project has one or more layers, False otherwise.
-    """
-
     instance = QgsProject.instance()
     assert instance is not None
     layers = instance.mapLayers()
@@ -27,7 +22,6 @@ def save_quick_osm_layers(output_directory: str) -> None:
     assert instance is not None
 
     for layer in instance.mapLayers().values():
-        # Check if the layer is a vector layer and a temporary memory layer
         if (
             isinstance(layer, QgsVectorLayer)
             and layer.isValid()
@@ -36,13 +30,10 @@ def save_quick_osm_layers(output_directory: str) -> None:
             geom_type = layer.geometryType()
             geom_type_str = ["point", "line", "polygon"][geom_type]
 
-            # Define the output file path and layer name
-            layer_name = layer.name()
-            new_layer_name = f"{layer_name}_{geom_type_str}"
+            new_layer_name = f"{layer.name()}_{geom_type_str}"
             layer.setName(new_layer_name)
             output_file = str(Path(output_directory) / f"{new_layer_name}.gpkg")
 
-            # Export the layer's style to a QML file
             qml_file = Path(output_directory) / f"{new_layer_name}.qml"
             layer.saveNamedStyle(str(qml_file))
 
@@ -56,17 +47,14 @@ def save_quick_osm_layers(output_directory: str) -> None:
                 layerOptions=["GEOMETRY_NAME=geom", f"layerName={new_layer_name}"],
             )
 
-            # Add the permanent layer to the QGIS project
             permanent_layer = QgsVectorLayer(
                 f"{output_file}|layername={new_layer_name}", new_layer_name, "ogr"
             )
             QgsProject.instance().addMapLayer(permanent_layer)
 
-            # Load the style from the QML file into the new layer
             permanent_layer.loadNamedStyle(str(qml_file))
 
-            # Optionally, remove the temporary layer from the project
+            # Delete the QML file to save storage space and remove the temporary layer
+            # from the project
             QgsProject.instance().removeMapLayer(layer.id())
-
-            # Delete the QML file to save storage space
             qml_file.unlink()
