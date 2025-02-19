83bd94ba7e3a2a46f6e0d0b07fc1dc35eb068ebb
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-02-19 11:34:09 +0000

    Fix tests
    Co-authored-by: Ben Whitehead (student) <2732382W@student.gla.ac.uk>

--- tests/core/test_layers.py
+++ tests/core/test_layers.py
@@ -1,24 +1,19 @@
 def test_get_layers_from_group() -> None:
-    from qgis.core import QgsLayerTreeGroup, QgsLayerTreeLayer, QgsVectorTileLayer
+    from qgis.core import QgsLayerTreeGroup, QgsLayerTreeLayer
 
     group = QgsLayerTreeGroup("test_group")
-    v1 = QgsVectorTileLayer(None, "vector_tile_1").id()
-    layer1 = QgsLayerTreeLayer(v1, "layer1", )
-    v2 = QgsVectorTileLayer(None, "vector_tile_2").id()
-    layer2 = QgsLayerTreeLayer(v2, "layer2")
-    non_layer = QgsLayerTreeLayer("non_layer")
-
+    layer1 = QgsLayerTreeLayer(None, "layer1")
+    layer2 = QgsLayerTreeLayer(None, "layer2")
+    non_layer = QgsLayerTreeGroup("non_layer")
     group.addChildNode(layer1)
     group.addChildNode(layer2)
     group.addChildNode(non_layer)
-
     from prettier_maps.core.layers import get_layers_from_group
 
     result = get_layers_from_group(group)
-
     assert len(result) == 2
-    assert result[0] == layer1.layer()
-    assert result[1] == layer2.layer()
+    assert result[0] == layer1
+    assert result[1] == layer2
 
 
 def test_filter_layers() -> None:
