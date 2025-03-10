866636374eb5f9d6e44de2d82823dbf90bf5ef29
Ben Whitehead <2732382W@student.gla.ac.uk>
2025-03-10 14:35:56 +0000

    Import correct renderer
    Includes cleanup, don't leave commented code blocks in the file. We use git for a reason.

--- tests/core/test_layers.py
+++ tests/core/test_layers.py
@@ -1,9 +1,9 @@
 from qgis.core import (
     Qgis,
-    QgsFeatureRenderer,
     QgsLayerTreeGroup,
     QgsLayerTreeLayer,
     QgsProject,
+    QgsSingleSymbolRenderer,
     QgsSymbol,
     QgsVectorLayer,
     QgsVectorTileBasicRenderer,
@@ -73,38 +73,6 @@ def test_filter_layers() -> None:
 
 def all_elements_equal(iterable) -> bool:
     return iterable.count(iterable[0]) == len(iterable)
-
-
-# def test_single_layer_styling() -> None:
-#     instance = QgsProject()
-#     assert instance is not None
-#     root = instance.layerTreeRoot()
-#     assert root is not None
-
-#     layer_tree = QgsLayerTreeLayer()
-#     root.addChildNode(layer_tree)
-
-#     layers = []
-#     for geom_type in [Qgis.GeometryType(i) for i in range(3)]:
-#         # problematic instantiation of layer
-#         layer = QgsVectorLayer()
-#         renderer = QgsFeatureRenderer()
-#         symbol = QgsSymbol.defaultSymbol(geom_type)
-
-#         renderer.setSymbol(symbol)
-#         layer.setRenderer(renderer)
-
-#         layers.append(layer)
-
-#     for layer in layers:
-#         style_single_layer(layer)
-
-#     colors = [layer.renderer().symbol().color() for layer in layers]
-
-#     assert all_elements_equal(colors)
-
-
-# test_single_layer_styling()
 
 
 def test_single_layer_styling() -> None:
