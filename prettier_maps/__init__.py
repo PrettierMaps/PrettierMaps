b275e21a67d904610f7e231460733456d7b272de
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-03-24 14:46:02 +0000

    Bump to 1.4.4

--- prettier_maps/__init__.py
+++ prettier_maps/__init__.py
@@ -1,7 +1,7 @@
 from .interfaces import IQgisInterface
 from .plugin import PrettierMapsPlugin
 
-__version__ = "1.4.3"
+__version__ = "1.4.4"
 
 
 def classFactory(iface: IQgisInterface) -> PrettierMapsPlugin:
